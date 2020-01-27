b975db361cd7bb4e59600f08d25ebf7790f11c99
ghnateesh <56351172+ghnateesh@users.noreply.github.com>
2020-01-27 08:45:41 +0530

    Update rusted_mail.rs

--- task_7/rusted_mail.rs
+++ task_7/rusted_mail.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("Enter your email id :");
     let mut b1:String=io::stdin().read_line(&mut arr).unwrap().to_string();
     let n: Regex = Regex::new(r"^[A-Za-z0-9!#$%&'*+=?^_`{|}~.]+@[a-zA-z0-9]+\.[a-z]*").unwrap();
-    let w=n.is_match( &mut line);
+    let w=n.is_match( &mut arr);
     if w{
         println!("valid")
         }
